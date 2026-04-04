d27a4910908bbbdd65b4b48f37bdc509a25508ed
Artyom Gavrilov <artyom.e.gavrilov@gmail.com>
2026-04-04 23:28:01 +0300

    feat: implement convolution for task1

---
+++ xmake.lua
@@ -0,0 +1,88 @@
+add_rules("mode.debug", "mode.release", "mode.coverage")
+add_rules("plugin.compile_commands.autoupdate", { outputdir = "build/" })
+
+set_toolchains("llvm")
+add_cxflags("-Wno-pre-c++20-compat", "-Wno-pre-c++17-compat", "-Wno-c++98-compat", "-Wno-c++98-compat-pedantic")
+add_cxflags("-Wno-reserved-identifier")
+
+set_languages("c++2c")
+set_warnings("everything", "error")
+add_cxxflags("-fexperimental-library")
+if not is_plat("windows") then
+    add_links("c++abi")
+end
+
+set_runtimes("c++_static")
+
+if is_mode("coverage") then
+    add_cxxflags("-fprofile-instr-generate", "-fcoverage-mapping")
+    add_ldflags("-fprofile-instr-generate")
+end
+
+if is_mode("ALUSan") then
+    set_symbols("debug")
+    set_optimize("fast") 
+    set_policy("build.sanitizer.address", true)
+    set_policy("build.sanitizer.leak", true)
+    set_policy("build.sanitizer.undefined", true)
+    add_cxflags("-fno-omit-frame-pointer -fno-optimize-sibling-calls")
+end
+
+-- TODO(metrapoliten): MSan fails on ut library: https://github.com/boost-ext/ut/issues/712.
+-- I couldn't fix or ignore the error. It might be worth switching to another library later.
+--[[ 
+if is_mode("MSan") then
+    set_symbols("debug")
+    set_optimize("fast") 
+    set_policy("build.sanitizer.memory", true)
+    add_cxflags("-fsanitize-memory-track-origins")
+    add_cxflags("-fno-omit-frame-pointer -fno-optimize-sibling-calls")
+    add_cxflags("-fsanitize-ignorelist=" .. path.join(os.projectdir(), "msan_ignore"), {force = true})
+    add_ldflags("-fsanitize-ignorelist=" .. path.join(os.projectdir(), "msan_ignore"))
+end
+]]
+
+if is_mode("TSan") then
+    set_symbols("debug")
+    set_optimize("fast") 
+    set_policy("build.sanitizer.thread", true)
+    add_cxflags("-fno-omit-frame-pointer -fno-optimize-sibling-calls")
+end
+
+option("hardening")
+    set_default(false)
+    set_showmenu(true)
+    set_description("Set libc++ debug hardening mode. For details: https://libcxx.llvm.org/Hardening.html")
+option_end()
+
+if has_config("hardening") then 
+    add_defines("_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_DEBUG")
+end
+
+target("conv")
+    set_kind("static")
+    add_sysincludedirs("deps", { private = true })
+    add_cxxflags("-Wno-padded")
+    add_files("src/*.cc")
+    add_files("src/*.ccm", { public = true })
+
+target("ut")
+    set_kind("static")
+    set_default(false)
+    set_warnings("none")
+    add_sysincludedirs("deps/ut", { private = true})
+    add_files("deps/ut/ut.cppm", { public = true })
+
+target("test")
+    set_kind("binary")
+    set_default(false)
+    add_deps("conv", "ut")
+    add_tests("default")
+    add_files("test/test.cc")
+    
+ target("cifuzz")
+    set_kind("binary")
+    set_default(false)
+    set_enabled(is_mode("cifuzz"))        
+    add_deps("conv")
+    add_files("test/fuzz.cc")
